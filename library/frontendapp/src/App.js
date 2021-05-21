3eb8df35e48282fe6e4942cddd8833efb1e036f6
xBarbarian <alexadr.jukov@gmail.com>
2021-05-21 16:45:01 +0300

    lesson 2 in progress

--- library/frontendapp/src/App.js
+++ library/frontendapp/src/App.js
@@ -7,34 +7,50 @@ import axios from 'axios'
 
 class App extends React.Component {
 
-   constructor(props) {
-       super(props)
-       this.state = {
-           'users': []
-       }
-   }
+    constructor(props) {
+        super(props)
+        this.state = {
+            'users': []
+        }
+    }
 
-   componentDidMount() {
-       axios.get('http://127.0.0.1:8000/api/users')
-    	.then(response => {
-    	    const users = response.data
-            this.setState(
-                {
-                    'users': users
-                }
-            )
-    	}).catch(error => console.log(error))
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/users')
+     	.then(response => {
+     	    const users = response.data
+             this.setState(
+                 {
+                     'users': users
+                 }
+             )
+     	}).catch(error => console.log(error))
 
 
-   }
+    }
+    // componentDidMount() {
+    //
+    //     const users = [
+    //         {
+    //             "username": "test",
+    //             "email": "test@test.ru",
+    //             "first_name": "Test",
+    //             "last_name": "Test"
+    //         }
+    //     ]
+    //     this.setState(
+    //         {
+    //             'users': users
+    //         }
+    //     );
+    // }
 
-   render () {
-       return (
-           <div>
-               <UserList users={this.state.users} />
-           </div>
-       )
-   }
+    render() {
+        return (
+            <div>
+                <UserList users={this.state.users}/>
+            </div>
+        )
+    }
 }
 
 
