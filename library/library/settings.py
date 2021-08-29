e482393dc809dec6e6beaf1b288c0092341b4ccd
xBarbarian <alexadr.jukov@gmail.com>
2021-08-29 23:55:41 +0300

    Lesson 11, add create method for todo and project.

--- library/library/settings.py
+++ library/library/settings.py
@@ -12,11 +12,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from django.conf.global_settings import AUTH_USER_MODEL
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+FRONTEND_DIR = BASE_DIR / 'libary/frontendapp'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -68,9 +71,7 @@ ROOT_URLCONF = 'library.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / '/frontendapp/build' ,
-        ],
+        'DIRS': [FRONTEND_DIR / 'build'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,10 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_DIRS = [FRONTEND_DIR / 'build' / 'static']
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    BASE_DIR / '/frontendapp/build/static',
-)
+
 
 GRAPHENE = {
     "SCHEMA": "library.schema.schema"
